3795a2468e3a31f0bdb3c49b7521a78e4b392616
Your Name <andrew.smiley@live.com>
2017-03-23 21:37:00 -0400

    Added some stuff

--- src/csc501/Main.java
+++ src/csc501/Main.java
@@ -1,5 +1,10 @@
 package csc501;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,5 +16,48 @@ public class Main {
          d. Merge
          e. Quick
          */
+        //this is our array of elements from the text file
+        Integer numbers[] = null;
+        //the number of items in the text file
+        int size = 0;
+
+        //attempt to open the file
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("input1.txt")));
+            //reference for our line
+            String line;
+
+            //the line count
+            int lineCount = 0;
+            while ((line = br.readLine()) != null) {
+
+                if (lineCount == 0) {
+                    size = Integer.parseInt(line);
+                    numbers = new Integer[size];
+                    lineCount++;
+
+                } else {
+                    //just add to numbers the line count. This is lineCount-2 because line 1 is the number of lines in the
+                    //file
+                    numbers[lineCount - 1] = Integer.parseInt(line);
+                    lineCount++;
+                }
+            }
+
+            //close the file
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Sorting.bubbleSort(numbers);
+//        for(int i : numbers){
+//            System.out.println(i);
+//        }
+
+        System.out.println("The size is: "+size);
+        System.out.println("Array Validity: "+ (Sorting.validate(numbers) ? "Valid" :"Invalid"));
     }
+
+
 }
--- src/csc501/Sorting.java
+++ src/csc501/Sorting.java
@@ -11,10 +11,12 @@ package csc501;
  */
 public class Sorting {
     /**
-     * This is our insertion sort method
-     * @param array
+     * This is our insertion sort method, we pass in the Integer array because this is java, we are working with passing by reference
+     * the array will be sorted when this method is complete
+     * @param array the array to sort
+     * @return int the number of the operations the sorting took
      */
-    public static void insertionSort(int[] array){
+    public static int insertionSort(Integer[] array){
         for (int i = 1; i < array.length; i++){
             int k = array[i]; // store the element we are currently trying to find a home for
             int j= i-1; // store the current index we are on in our outer loop, i-1 because i is the index of the element we want
@@ -27,5 +29,54 @@ public class Sorting {
             }
             array[j+1]=k; //finally, give k a home
         }
+        return 0;
+    }
+
+    /**
+     * This is our bubble sort method. Takes an integer array and returns the number of operations in the sort
+     * @param array the array to sort
+     * @return int the number of operations to execute the sort
+     */
+    public static int bubbleSort(Integer[] array){
+        int n = array.length; //the length of the array
+        int temp = 0; // the bubble item
+        //our outer loop. iterate through the array from 0=>n-1
+        for (int i = 0; i < n; i++) {
+            /*our inner loop which allows us to bubble up the values
+             *the n-i part decrements on each pass of the outer loop because on each completion of the inner loop
+             * n-i is in the correct position
+             */
+            for (int j = 1; j < (n - i); j++) {
+                //check to see if we need to bubble up
+                if (array[j - 1] > array[j]) {
+                    temp = array[j - 1]; //if we do, store the value we need to bubble
+                    array[j - 1] = array[j];// move the item that is < our current element to the position of our current element
+                    array[j] = temp; //bubble the current element
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Our selection sort method
+     * @param array
+     * @return
+     */
+    public static int selectionSort(Integer[] array){
+
+        return 0;
+    }
+
+
+    public static boolean validate(Integer[] array){
+        boolean valid = true;
+        int val = array[0];
+        for (int i = 1; i < array.length; i++){
+            if(val > array[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
